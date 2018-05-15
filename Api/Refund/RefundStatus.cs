ab3493144dffba720430dcf09e7317936e128339
arst <artem.startsev@gmail.com>
2018-05-15 07:22:10 +0300

    Cleanup after refactoring

--- Api/Refund/RefundStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nop.Plugin.Payments.PayU.Api.Refund
-{
-    public class RefundStatus
-    {
-        public string StatusCode { get; set; }
-        public string Severity { get; set; }
-        public string StatusDescription { get; set; }
-    }
-}
